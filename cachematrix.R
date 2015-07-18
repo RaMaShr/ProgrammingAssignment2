8007462470ed3e04238651d90856662db9b5ddbd
RaMaShr <RaMaShr@users.noreply.github.com>
2015-07-18 17:23:48 +0100

    Defined cache matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Defines a class that stores a matrix and allows for the inverse to of the matrix to be cached.
 
-## Write a short comment describing this function
+## Creates class to store matrix and cached inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(MatrixInverse) inverse <<- MatrixInverse
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
+    
 
-
-## Write a short comment describing this function
+## Checks if cached inverse for matrix exists. Outputs cached inverse if so, else calculates, caches and outputs 
+## matrix inverse
 
 cacheSolve <- function(x, ...) {
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
         ## Return a matrix that is the inverse of 'x'
 }
